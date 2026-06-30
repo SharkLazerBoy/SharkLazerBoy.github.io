a164b39dc226461ef844dee6399e39f4e86f902f
SharkLazerBoy <142265533+SharkLazerBoy@users.noreply.github.com>
2026-06-30 13:58:14 -0400

    Add personal information logging to jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,4 @@
 console.log("This is loaded from the external js"); 
+console.log("Name: Bryce Fain");
+console.log("School: Corvian High School");
+console.log("Hobby: Reading and writing stories");
