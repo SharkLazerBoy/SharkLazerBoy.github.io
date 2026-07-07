164ec25c73e66e8adf97df023ddcfc4eeb55dac2
SharkLazerBoy <142265533+SharkLazerBoy@users.noreply.github.com>
2026-07-07 14:11:40 -0400

    Update basicmap.js

--- basicmap.js
+++ basicmap.js
@@ -1,4 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+//35.227° N latitude, 80.843° W longitude
+var charlotte = [35.2271, -80.8431]
+var map = L.map('map').setView(charlotte, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
