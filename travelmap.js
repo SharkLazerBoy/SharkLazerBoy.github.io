5e68f92e489c92cf5dcb811e2ddeda87cd3b238d
SharkLazerBoy <142265533+SharkLazerBoy@users.noreply.github.com>
2026-07-09 14:09:01 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -11,5 +11,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from England!</p>").addTo(map);
-var marker = L.marker(londonBridge).bindPopup("<p>Hello, from England!</p>").addTo(map);
-var marker = L.marker(stonehedge).bindPopup("<p>Hello, from England!</p>").addTo(map);
+var marker1 = L.circle(londonBridge,{color: "black", fillColor: "408f55", radius:80}).addTop(map);
+var marker2 = L.marker(stonehedge).bindPopup("<p>Hello, from England!</p>").addTo(map);
