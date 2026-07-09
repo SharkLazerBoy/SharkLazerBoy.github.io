a4faa7ef487653067419c9739c9fc1680344a835
SharkLazerBoy <142265533+SharkLazerBoy@users.noreply.github.com>
2026-07-09 13:58:27 -0400

    Add markers for London Bridge and Stonehenge

--- travelmap.js
+++ travelmap.js
@@ -1,8 +1,15 @@
 //52.35552° N latitude and 1.17432° E longitude
+//london bridge 51.50771° N, 0.08786° W
+//stonehedge 51.178844, -1.826323.
+//
 var coordinates = [52.35552, 1.17432]
+var londonBridge = [51.50771, 0.08786]
+var stonehedge = [51.178844, -1.826323]
 var map = L.map('map').setView(coordinates, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from England!</p>").addTo(map);
+var marker = L.marker(londonBridge).bindPopup("<p>Hello, from England!</p>").addTo(map);
+var marker = L.marker(stonehedge).bindPopup("<p>Hello, from England!</p>").addTo(map);
